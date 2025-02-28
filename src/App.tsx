7172f8d18629f292609c42450b031047f89f38f7
Victor Bruce <victorbruce82@gmail.com>
2025-02-28 04:58:31 +0000

    style: add framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-400 mb-4">⚛️ React Boilerplate</h1>
-      <p>Status: 🟢 In progress.</p>
+      <motion.h1
+        className="text-blue-400 mb-4"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        ⚛️ React Boilerplate
+      </motion.h1>
+      <motion.p initial={{ y: 20 }} animate={{ y: 0 }}>
+        Status: 🟢 In progress.
+      </motion.p>
     </>
   );
 }
