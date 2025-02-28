e1d260199d2bb6c1b2db4cac4bfcc404b55addda
Victor Bruce <victorbruce82@gmail.com>
2025-02-28 04:52:40 +0000

    style: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>⚛️ React Boilerplate</h1>
+      <h1 className="text-blue-400 mb-4">⚛️ React Boilerplate</h1>
       <p>Status: 🟢 In progress.</p>
     </>
   );
