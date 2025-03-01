de7ce97dc89a245b90ce7a4e02e2eae3e6215bb8
Victor Bruce <victorbruce82@gmail.com>
2025-03-01 22:39:13 +0000

    chore: refactor components and update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,21 @@
-import { describe, it } from "vitest";
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import App from "./App";
 
-describe('App', () => {
-  it('renders the App component', () => {
-    render(<App />)
-    
-    screen.debug(); // prints out the jsx in the App component unto the command line
-  })
-})
+describe("App", () => {
+  it("renders the App component", () => {
+    // arrange
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    // act
+    let text = screen.getByText("⚛️ React Boilerplate");
+
+    // assert
+    expect(text).toBeInTheDocument();
+  });
+});
